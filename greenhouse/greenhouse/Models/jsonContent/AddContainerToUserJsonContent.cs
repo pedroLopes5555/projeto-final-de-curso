c382b1d9976493637ff249ed3db3eef42a79b54a
pedroLopes5555 <pedrosilvestrelopes@hotmail.com>
2024-05-23 14:10:25 +0100

    added json content

---
+++ greenhouse/greenhouse/Models/jsonContent/AddContainerToUserJsonContent.cs
@@ -0,0 +1,14 @@
+﻿using greenhouse.DB;
+
+namespace greenhouse.Models.jsonContent
+{
+    public class AddContainerToUserJsonContent
+    {
+        public String name { get; set; }
+        public String location { get; set; }
+
+        public Guid userId { get; set; }
+
+    }
+
+}
