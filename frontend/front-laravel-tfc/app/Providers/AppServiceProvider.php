f0c8ee1a475211e778e8c4255119a01dec2be15e
diomferreira <diomferreira@gmail.com>
2024-06-28 22:13:07 +0100

    alter https protocol

--- frontend/front-laravel-tfc/app/Providers/AppServiceProvider.php
+++ frontend/front-laravel-tfc/app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot()
     {
-        //
+        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'local') {
+            URL::forceScheme('https');
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function register()
     {
         //
     }
 }
+
